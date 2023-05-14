b7b59f5dda308cb11fa836afae56b3da474c6242
Sachinrawat1215 <Sachinrawat0853@gmail.com>
2023-05-15 02:15:23 +0530

    change p tag text

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Deploying this app using CI/CD
         </p>
         <a
           className="App-link"
